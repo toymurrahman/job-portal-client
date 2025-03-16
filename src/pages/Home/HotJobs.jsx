64ac8874888ca12453f0c1845ced3d88f6314623
toymurrahman <rahmantoymur2@gmail.com>
2025-03-17 00:07:15 +0600

    Jobs Card create

--- src/pages/Home/HotJobs.jsx
+++ src/pages/Home/HotJobs.jsx
@@ -1,29 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import HotJobsCard from './HotJobsCard';
+import React, { useEffect, useState } from "react";
+import HotJobsCard from "./HotJobsCard";
 
 const HotJobs = () => {
+  const [jobs, setJobs] = useState([]);
 
-    const [jobs,setJobs] = useState([]);
+  // Fetching data from API
+  useEffect(() => {
+    fetch("http://localhost:3000/jobs")
+      .then((response) => response.json())
+      .then((data) => setJobs(data));
+  }, []);
 
-    // Fetching data from API
-    useEffect(() => {
-        fetch('https://http://localhost:3000/jobs')
-       .then(response => response.json())
-       .then(data => setJobs(data))
-    }
-    ,[]);
+  return (
+    <div className="container mx-auto px-4 pt-4">
+      <div className="p-10">
+        <h1 className="text-3xl font-bold text-center mt-10">Hot Jobs</h1>
+        <p className="text-gray-600 text-center">
+          Find the best opportunities for your career now
+        </p>
+      </div>
+      <br />
 
-    return (
-        <div>
-            <div>
-                {
-                    jobs.map(job => (
-                        <HotJobsCard key={job._id} job={job} />
-                    ))
-                }
-            </div>
-        </div>
-    );
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {jobs.map((job) => (
+          <HotJobsCard key={job._id} job={job} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default HotJobs;
--- src/pages/Home/HotJobsCard.jsx
+++ src/pages/Home/HotJobsCard.jsx
@@ -1,11 +1,69 @@
-import React from 'react';
+import React from "react";
+import { FaMapMarkerAlt } from "react-icons/fa";
 
-const HotJobsCard = () => {
-    return (
-        <div>
-            
+const HotJobsCard = ({ job }) => {
+  const {
+    title,
+    location,
+    jobType,
+    category,
+    company,
+    requirements,
+    company_logo,
+    salaryRange,
+    description,
+    status,
+    applicationDeadline
+  } = job;
+  return (
+    <div className="card bg-base-100 shadow-sm">
+      <div className="flex gap-2 p-4">
+      <figure>
+        <img
+            className="w-16"
+          src={company_logo}
+          alt="Company Logo"
+        />
+      </figure>
+      <div>
+        <h4 className="text-xl">{company}</h4>
+        <h4 className="flex gap-1 items-center text-sm">  <FaMapMarkerAlt />  {location}</h4>
+      </div>
+      {/* <div className="flex gap-2 items-center ml-auto">
+      <div className="status status-info animate-bounce"> </div> 
+        <span className="text-sm text-gray-500">{status}</span>
+      </div> */}
+      </div>
+      <div className="flex flex-wrap gap-2 ml-5">
+        <span className="badge badge-dash badge-primary"> {jobType} </span>
+        <span className="badge badge-dash badge-accent"> {category} </span>
+        <span className="badge badge-dash badge-secondary"> {status} </span>
+      </div>
+      <div className="card-body">
+        <h2 className="card-title">  {title} </h2>
+        <p>
+          {description}
+        </p>
+        <div className="flex gap-2 flex-wrap">
+            {
+                requirements.map((req, index) => (
+                    <span key={index} className="badge badge-soft badge-primary gap-2">{req}</span>
+                ))
+            }
         </div>
-    );
+            <div className="badge  text-xs">Application Deadline:    {applicationDeadline} </div>
+        <div className="flex items-center justify-between ">
+        <div className="">
+          <h4 className="text-xs "> Salary: {salaryRange.min} - {salaryRange.max} BDT </h4>          
+        </div>
+        <div className=" ">
+          <button className="btn btn-primary">Apply</button>
+        </div>
+        </div>
+     
+      </div>
+    </div>
+  );
 };
 
 export default HotJobsCard;
